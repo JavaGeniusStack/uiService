80ff2d7d04bd3e222a711d9c9fee5e43667d9744
JavaGeniusStack <amarbhorey@gmail.com>
2026-02-09 20:21:17 +0530

    implemented till bootstrap

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
-  standalone: false,
+  standalone: true,
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.css'
+  styleUrl: './welcome.component.css',
+  imports: [NgIf, RouterLink]
 })
 export class WelcomeComponent implements OnInit {
-  message : string = 'Welcome to the Todo Manager App Created By AMAR BHORE!';
-  constructor() { }
+
+  message: string = 'Welcome  to the Todo Manager App empowered with Eissenhower Matrix!';
+  name: string = '';
+  constructor(private route: ActivatedRoute) { }
+
   ngOnInit(): void {
+    // console.log(this.route.snapshot.paramMap.get('username'));
+    this.name = this.route.snapshot.paramMap.get('username') || '';
     console.log(this.message);
   }
 }
